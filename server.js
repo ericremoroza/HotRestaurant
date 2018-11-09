121ea85cf725b333b5070071675f5f14a2afdc5e
ericremoroza <eric.remoroza@outlook.com>
2018-11-08 21:14:52 -0500

    more app.get functions

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
 app.listen(PORT, function() {
     console.log("Hot Restaurant");
     console.log("http://localhost:" + PORT);
